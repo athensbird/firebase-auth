e335150f35e54b46e947807954685d2bba93fa65
athensbird <popularwatson@gmail.com>
2018-02-03 18:11:19 -0600

    configured for redux

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
-const Navigation = (props, { authUser }) =>
+const Navigation = ({ authUser }) =>
   <div>
     { authUser
         ? <NavigationAuth />
         : <NavigationNonAuth />
     }
   </div>
-
-Navigation.contextTypes = {
-  authUser: PropTypes.object
-};
 
 const NavigationAuth = () =>
   <ul>
@@ -31,5 +27,8 @@ const NavigationNonAuth = () =>
     <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
   </ul>
 
+const mapStateToProps = (state) => ({
+  authUser: state.sessionState.authUser,
+});
 
-export default Navigation;
+export default connect(mapStateToProps)(Navigation);
